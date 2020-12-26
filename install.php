d68816c6c72c571a33270c510f8819cfdbfd69c5
vladlen <vladlengg@gmail.com>
2020-12-26 12:18:39 +0500

    add procrm_active_subscribes table

--- install.php
+++ install.php
@@ -3,6 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $CI = &get_instance();
 
+// Категория абонементов
 if (!$CI->db->table_exists(db_prefix() . 'procrm_subscribes_categories')) {
     $CI->db->query('CREATE TABLE `' . db_prefix() . "procrm_subscribes_categories` (
   `id` int(11) NOT NULL,
@@ -16,6 +17,7 @@ if (!$CI->db->table_exists(db_prefix() . 'procrm_subscribes_categories')) {
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT');
 }
 
+// Абонементы
 if (!$CI->db->table_exists(db_prefix() . 'procrm_subscribes')) {
     $CI->db->query('CREATE TABLE `' . db_prefix() . "procrm_subscribes` (
   `id` int(11) NOT NULL,
@@ -31,5 +33,22 @@ if (!$CI->db->table_exists(db_prefix() . 'procrm_subscribes')) {
   ADD PRIMARY KEY (`id`);');
 
     $CI->db->query('ALTER TABLE `' . db_prefix() . 'procrm_subscribes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT');
+}
+
+// Активные абонементы
+if (!$CI->db->table_exists(db_prefix() . 'procrm_active_subscribes')) {
+    $CI->db->query('CREATE TABLE `' . db_prefix() . "procrm_active_subscribes` (
+  `id` int(11) NOT NULL,
+  `subscribe_id` int(11) NOT NULL,
+  `lead_id` int(11) NOT NULL,
+  `used_frost_days` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=" . $CI->db->char_set . ';');
+
+    $CI->db->query('ALTER TABLE `' . db_prefix() . 'procrm_active_subscribes`
+  ADD PRIMARY KEY (`id`);');
+
+    $CI->db->query('ALTER TABLE `' . db_prefix() . 'procrm_active_subscribes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT');
 }
